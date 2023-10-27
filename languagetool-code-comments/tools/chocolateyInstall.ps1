94a2246f7de5936dc85381a11b823e362f991030
Dustin Blackman <dev@dustinblackman.com>
2023-10-27 12:48:54 -0400

    Add languagetool-code-comments 0.6.0

--- languagetool-code-comments/tools/chocolateyInstall.ps1
+++ languagetool-code-comments/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64bit       = 'https://github.com/dustinblackman/languagetool-code-comments/releases/download/v0.5.0/languagetool-code-comments_0.5.0_windows_amd64.zip'
-    checksum64     = '563126b4a8e380d861a5eabd8f64359d752d3bfb204ac94fa8ddd7df5d06f86a'
+    url64bit       = 'https://github.com/dustinblackman/languagetool-code-comments/releases/download/v0.6.0/languagetool-code-comments_0.6.0_windows_amd64.zip'
+    checksum64     = 'b10dba32d668f236e154a0fd1a8caa78d16963a7e8971650e258f5dd8cc70386'
     checksumType64 = 'sha256'
 }
 
