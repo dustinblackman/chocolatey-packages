7a8e923b391557c95fca6f0334650fe2718b89dc
Dustin Blackman <dev@dustinblackman.com>
2023-11-09 07:22:44 -0500

    fix: TTY when remote deploying

--- build-deploy.sh
+++ build-deploy.sh
@@ -6,4 +6,4 @@ export DOCKER_DEFAULT_PLATFORM=linux/amd64
 APP="$1"
 
 docker build -t choco-deploy:local .
-docker run -it --rm -v "$PWD/$APP:/app" -w "/app" -e "CHOCOLATEY_KEY=${CHOCOLATEY_KEY}" choco-deploy:local /usr/bin/publish.sh
+docker run -i --rm -v "$PWD/$APP:/app" -w "/app" -e "CHOCOLATEY_KEY=${CHOCOLATEY_KEY}" choco-deploy:local /usr/bin/publish.sh
