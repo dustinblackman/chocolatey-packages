0c22bde7c22d64d145adcfa1f293482f0a6b10b2
Dustin Blackman <dev@dustinblackman.com>
2023-11-20 22:54:59 -0500

    Add oatmeal 0.3.0

--- oatmeal/tools/chocolateyInstall.ps1
+++ oatmeal/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64bit       = 'https://github.com/dustinblackman/oatmeal/releases/download/v0.2.5/oatmeal_0.2.5_windows_amd64.zip'
-    checksum64     = '6c1e5bd250a2417088280ec5dea823bec446bc000a1d8cfcde14456357e9d989'
+    url64bit       = 'https://github.com/dustinblackman/oatmeal/releases/download/v0.3.0/oatmeal_0.3.0_windows_amd64.zip'
+    checksum64     = 'f96471ad8a949bf1c1fc2d7b9299acca8ed20f13c7245c0620b2edd5ea83a562'
     checksumType64 = 'sha256'
 }
 
